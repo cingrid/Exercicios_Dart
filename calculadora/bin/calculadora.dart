f2df0d6a8bd47a7f1b96b8caf1ae13ba16b36c8e
cingrid <devingridmunhoz@gmail.com>
2025-12-12 14:18:20 -0300

    add exercicio 4 e novo projeto

--- calculadora/bin/calculadora.dart
+++ calculadora/bin/calculadora.dart
@@ -5,15 +5,15 @@ void main() {
   double numeroUm = 0; // converte string p double
   double numDois = 0; // converte string p double
   String operacao = ""; 
-
-
-void soma(){
-  print(numeroUm + numDois);
-}
-
+  String? entrada = "";
+  List<String> operacoes = <String>["+", "-", "*", "/"]; //tipando a lista com string. declarando a lista
+  
 
 /* escopo é fechado. a funcao soma só tem acesso as variaveis numeroUm e numDois, 
 porque ela foi declarada dentro de void.*/
+void soma(){
+  print(numeroUm + numDois);
+}
 
 void subtracao(){
   print(numeroUm - numDois);
@@ -23,11 +23,11 @@ void divisao(){
   print(numeroUm / numDois);
 }
 
-
 void multiplicacao(){
   print(numeroUm * numDois);
 }
 
+// metodos
 void calcular(){
   switch(operacao) {
   case "+": 
@@ -48,22 +48,38 @@ void calcular(){
 print("Digite o primeiro valor: ");
 
 //tratamento de erro para casos de valores nulos
-//atribuindo um novo stdin as variaveis de numUm, numDis e operacao 
+//atribuindo um novo stdin as variaveis de numUm, numDois e operacao 
 // variavel de entrada. string que pode vir nula.
-String? entrada = stdin.readLineSync();
+entrada = stdin.readLineSync();
+
 if (entrada != null){
   if (entrada != ""){
     numeroUm = double.parse(entrada);
   }
 }
 
-print("Digite uma operação: ");
+// validações extra: operação. valida se ela existe 
+// utilizaremos uma lista de strings
+void getOperacao(){
+  print("Digite uma operação ${operacoes.toString()}");
+  entrada = stdin.readLineSync();
+  if (entrada != null){
+    // função contains verifica se a entrada existe na lista
+    if (operacoes.contains(entrada)){
+      operacao = entrada!;
+    } else {
+      print("Operação inválida.");
+      getOperacao();//recursão. chamamos a funcao dentro dela mesma. ponto de parada para parar o loop infinito.
+      
+    }
+  }
+}
 
-entrada = stdin.readLineSync();
+getOperacao();
 
 if (entrada != null){
   if (entrada != ""){
-    operacao = entrada; 
+    operacao = entrada!; 
   }
 }
 
@@ -72,12 +88,14 @@ print("Digite o segundo valor: ");
 entrada = stdin.readLineSync();
 if (entrada != null){
   if (entrada != ""){
-    numDois = double.parse(entrada);
+    numDois = double.parse(entrada!);
   }
 }
 
 print("O resultado da operação é: ");
 
 calcular();
+
+
 
 }
